ae14e1b12d6759bcf076159baf037f145b9d8b13
ryckmv <rick_mv@hotmail.com>
2024-07-10 00:14:29 -0300

    Create anagram.py

---
+++ exercicios/anagram.py
@@ -0,0 +1,16 @@
+def sao_anagrma(a,b):
+
+
+        for j in range(len(a)):
+            for p in range(len(b)):
+                  print(b)
+                
+            
+    
+         
+
+
+
+
+
+print(sao_anagrma('zorra','carro'))
